8d3a31fc80363b54612e7e1d38ce805ad4355715
Degarashi <degarasu@gmail.com>
2024-12-09 14:10:14 +0900

    [update] register select_modernprimitive operator

--- __init__.py
+++ __init__.py
@@ -2,8 +2,10 @@ if "bpy" in locals():
     import importlib
 
     importlib.reload(modern_primitive)  # noqa: F821
+    importlib.reload(modifier_select)  # noqa: F821
 else:
     from . import modern_primitive
+    from . import modifier_select
 
 import bpy # noqa: F401
 
@@ -11,8 +13,10 @@ import bpy # noqa: F401
 def register():
     print("-------------ModernPrimitive::register()--------------")
     modern_primitive.register()
+    modifier_select.register()
 
 
 def unregister():
     print("-------------ModernPrimitive::unregister()-----------")
     modern_primitive.unregister()
+    modifier_select.unregister()
