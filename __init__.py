475cc3b67c54e400d5f6c5c618c8e3303185870e
Degarashi <degarasu@gmail.com>
2024-12-12 19:34:48 +0900

    [update] move ConvertToCube operator to "convert" module

--- __init__.py
+++ __init__.py
@@ -3,20 +3,24 @@ if "bpy" in locals():
 
     importlib.reload(modern_primitive)  # noqa: F821
     importlib.reload(modifier_select)  # noqa: F821
+    importlib.reload(convert)  # noqa: F821
 else:
     from . import modern_primitive
     from . import modifier_select
+    from . import convert
 
-import bpy # noqa: F401
+import bpy  # noqa: F401
 
 
 def register():
     print("-------------ModernPrimitive::register()--------------")
     modern_primitive.register()
     modifier_select.register()
+    convert.register()
 
 
 def unregister():
     print("-------------ModernPrimitive::unregister()-----------")
     modern_primitive.unregister()
     modifier_select.unregister()
+    convert.unregister()
---
+++ convert.py
@@ -0,0 +1,103 @@
+import bpy
+from bpy.types import (
+    Object,
+    Operator,
+    Context,
+    NodeGroup,
+    NodeGroupInput,
+    NodesModifier,
+)
+from typing import cast
+from .aux_func import get_bound_box, get_object_just_added
+from typing import Any, Iterable
+
+
+def find_group_input(node_group: NodeGroup) -> NodeGroupInput:
+    for node in node_group.nodes:
+        if node.type == "GROUP_INPUT":
+            return node
+    raise KeyError("Group Input")
+
+
+def find_interface_name(node_group: NodeGroup, name: str) -> str:
+    gi = find_group_input(node_group)
+    for o in gi.outputs:
+        if o.name == name:
+            return o.identifier
+    raise KeyError(name)
+
+
+def set_interface_value(mod: NodesModifier, data: tuple[str, Any]) -> None:
+    sock_name = find_interface_name(mod.node_group, data[0])
+    mod[sock_name] = data[1]
+
+
+def set_interface_values(
+    mod: NodesModifier, context: Context, data: Iterable[tuple[str, Any]]
+) -> None:
+    for d in data:
+        set_interface_value(mod, d)
+    mod.node_group.interface_update(context)
+
+
+class ConvertToCube_Operator(Operator):
+    """Make Modern Cube From Object"""
+
+    bl_idname = "mesh.convert_to_modern_cube"
+    bl_label = "Convert object to ModernCube"
+
+    @classmethod
+    def poll(cls, context: Context | None) -> bool:
+        if context is None:
+            return False
+        context = cast(Context, context)
+
+        sel = context.selected_objects
+        if len(sel) == 0:
+            return False
+        for obj in sel:
+            if obj is None or obj.type != "MESH":
+                return False
+        return True
+
+    def _make_cube(self, context: Context, from_obj: Object) -> None:
+        (b_min, b_max) = get_bound_box(from_obj.bound_box)
+        center = (b_min + b_max) / 2
+
+        bpy.ops.mesh.make_modern_cube()
+        cube = get_object_just_added(context)
+
+        set_interface_values(
+            cube.modifiers[0],
+            context,
+            (
+                ("SizeX", (b_max.x - b_min.x) / 2),
+                ("SizeY", (b_max.y - b_min.y) / 2),
+                ("SizeZ", (b_max.z - b_min.z) / 2),
+            ),
+        )
+        cube.matrix_world = from_obj.matrix_world
+        cube.location = from_obj.matrix_world @ center
+
+    def execute(self, context: Context | None) -> set[str]:
+        for obj in context.selected_objects:
+            self._make_cube(context, obj)
+        return {"FINISHED"}
+
+
+MENU_TARGET = bpy.types.VIEW3D_MT_object_convert
+
+
+def menu_func(self, context: Context) -> None:
+    layout = self.layout
+    layout.operator(ConvertToCube_Operator.bl_idname, text="Modern Cube", icon="CUBE")
+
+
+def register() -> None:
+    bpy.utils.register_class(ConvertToCube_Operator)
+    MENU_TARGET.append(menu_func)
+
+
+def unregister() -> None:
+    MENU_TARGET.remove(menu_func)
+    bpy.utils.unregister_class(ConvertToCube_Operator)
