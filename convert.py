2d26d6bde604fa4a69df16306809add0d78b4eb8
Degarashi <degarasu@gmail.com>
2024-12-12 23:43:50 +0900

    [update] ConvertToCube: make deformable-cube option

--- convert.py
+++ convert.py
@@ -10,7 +10,7 @@ from bpy.types import (
 from typing import cast
 from .aux_func import get_bound_box, get_object_just_added
 from typing import Any, Iterable
-from bpy.props import BoolProperty
+from bpy.props import BoolProperty, EnumProperty
 
 
 def find_group_input(node_group: NodeGroup) -> NodeGroupInput:
@@ -49,6 +49,14 @@ class ConvertToCube_Operator(Operator):
     bl_options = {"REGISTER", "UNDO"}
 
     keep_original: BoolProperty(name="Keep Original", default=False)
+    cube_type: EnumProperty(
+        name="Cube Type",
+        default="Cube",
+        items=[
+            ("Cube", "Cube", "Make Normal Cube"),
+            ("DeformableCube", "Deformable Cube", "Make Deformable Cube"),
+        ],
+    )
 
     @classmethod
     def poll(cls, context: Context | None) -> bool:
@@ -68,18 +76,36 @@ class ConvertToCube_Operator(Operator):
         (b_min, b_max) = get_bound_box(from_obj.bound_box)
         center = (b_min + b_max) / 2
 
-        bpy.ops.mesh.make_modern_cube()
+        if self.cube_type == "Cube":
+            bpy.ops.mesh.make_modern_cube()
+        else:
+            bpy.ops.mesh.make_modern_deformablecube()
+
         cube = get_object_just_added(context)
 
-        set_interface_values(
-            cube.modifiers[0],
-            context,
-            (
-                ("SizeX", (b_max.x - b_min.x) / 2),
-                ("SizeY", (b_max.y - b_min.y) / 2),
-                ("SizeZ", (b_max.z - b_min.z) / 2),
-            ),
-        )
+        if self.cube_type == "Cube":
+            set_interface_values(
+                cube.modifiers[0],
+                context,
+                (
+                    ("SizeX", (b_max.x - b_min.x) / 2),
+                    ("SizeY", (b_max.y - b_min.y) / 2),
+                    ("SizeZ", (b_max.z - b_min.z) / 2),
+                ),
+            )
+        else:
+            set_interface_values(
+                cube.modifiers[0],
+                context,
+                (
+                    ("MinX", -b_min.x),
+                    ("MaxX", b_max.x),
+                    ("MinY", -b_min.y),
+                    ("MaxY", b_max.y),
+                    ("MinZ", -b_min.z),
+                    ("MaxZ", b_max.z),
+                ),
+            )
         cube.matrix_world = from_obj.matrix_world
         cube.location = from_obj.matrix_world @ center
         cube.name = from_obj.name + "_converted"
