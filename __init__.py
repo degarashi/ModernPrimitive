46a91f8bdb3c696ca50e46cb83ed3ee6b6b2ce80
Degarashi <degarasu@gmail.com>
2025-02-03 15:19:25 +0900

    [update] refactoring

--- __init__.py
+++ __init__.py
@@ -1,66 +1,54 @@
-if "bpy" in locals():
+def import_modules(mod_name: list[str]):
+    import importlib.util
+
+    modules = {}
+    for name in mod_name:
+        modules[name] = importlib.import_module(f".src.{name}", package=__package__)
+    return modules
+
+
+def reload_modules(mod_name: list[str]):
     import importlib
 
-    importlib.reload(modern_primitive)  # noqa: F821
-    importlib.reload(modifier_select)  # noqa: F821
-    importlib.reload(cube)  # noqa: F821
-    importlib.reload(version)  # noqa: F821
-    importlib.reload(panel)  # noqa: F821
-    importlib.reload(check_editmesh)  # noqa: F821
-    importlib.reload(wireframe)  # noqa: F821
-    importlib.reload(switch_wireframe) # noqa: F821
-    importlib.reload(apply_scale) # noqa: F821
-    importlib.reload(preference) # noqa: F821
-    importlib.reload(hud_draw) # noqa: F821
-    importlib.reload(restore_default)  # noqa: F821
-    importlib.reload(convert)  # noqa: F821
-else:
-    from .src import modern_primitive
-    from .src import modifier_select
-    from .src import cube
-    from .src import version
-    from .src import panel
-    from .src import check_editmesh
-    from .src import wireframe
-    from .src import switch_wireframe
-    from .src import apply_scale
-    from .src import preference
-    from .src import hud_draw
-    from .src import restore_default
-    from .src import convert
+    modules = {}
+    for name in mod_name:
+        modules[name] = importlib.reload(globals()[name])
+    return modules
 
-import bpy  # noqa: F401
+
+MODULE_NAMES: list[str] = [
+    "modern_primitive",
+    "modifier_select",
+    "cube",
+    "version",
+    "panel",
+    "check_editmesh",
+    "wireframe",
+    "switch_wireframe",
+    "apply_scale",
+    "preference",
+    "hud_draw",
+    "restore_default",
+    "convert",
+]
+
+modules = (
+    import_modules(MODULE_NAMES)
+    if "bpy" not in locals()
+    else reload_modules(MODULE_NAMES)
+)
+
+
+import bpy  # noqa: F401, E402
 
 
 def register():
     print("-------------ModernPrimitive::register()--------------")
-    modern_primitive.register()
-    modifier_select.register()
-    cube.register()
-    version.register()
-    panel.register()
-    check_editmesh.register()
-    wireframe.register()
-    switch_wireframe.register()
-    apply_scale.register()
-    preference.register()
-    hud_draw.register()
-    restore_default.register()
-    convert.register()
+    for module in modules.values():
+        module.register()
 
 
 def unregister():
     print("-------------ModernPrimitive::unregister()-----------")
-    modern_primitive.unregister()
-    modifier_select.unregister()
-    cube.unregister()
-    version.unregister()
-    panel.unregister()
-    check_editmesh.unregister()
-    wireframe.unregister()
-    switch_wireframe.unregister()
-    apply_scale.unregister()
-    preference.unregister()
-    hud_draw.unregister()
-    restore_default.unregister()
-    convert.unregister()
+    for module in modules.values():
+        module.unregister()
