b8d50f124f4671471dce0d88c7c148d8639df893
Degarashi <degarasu@gmail.com>
2025-03-08 21:21:45 +0900

    [add] MinMax class

--- src/aux_math.py
+++ src/aux_math.py
@@ -1,6 +1,49 @@
 import math
+from collections.abc import Iterable
 from math import isclose as m_isclose
-from mathutils import Vector, Quaternion
+from sys import float_info
+from typing import NamedTuple
+
+from bpy.types import Object
+from mathutils import Quaternion, Vector
+
+
+def make_vec3(val: float) -> Vector:
+    return Vector([val] * 3)
+
+
+class MinMax(NamedTuple):
+    min: Vector
+    max: Vector
+
+    @property
+    def average(self) -> Vector:
+        return (self.min + self.max) / 2
+
+    @property
+    def size(self) -> Vector:
+        return self.max - self.min
+
+    @staticmethod
+    def update_obj(obj: Object) -> None:
+        if obj.mode != "OBJECT":
+            obj.mode = "OBJECT"
+            obj.update_from_editmode()
+
+    @staticmethod
+    def from_iterable(verts: Iterable[Iterable[float]]):
+        min_v = make_vec3(float_info.max)
+        max_v = make_vec3(-float_info.max)
+        for pos in verts:
+            for i in range(3):
+                min_v[i] = min(min_v[i], pos[i])
+                max_v[i] = max(max_v[i], pos[i])
+        return MinMax(min_v, max_v)
+
+    @classmethod
+    def from_obj_bb(cls, obj: Object):
+        cls.update_obj(obj)
+        return MinMax.from_iterable(obj.bound_box)
 
 
 def is_close(*args) -> bool:
