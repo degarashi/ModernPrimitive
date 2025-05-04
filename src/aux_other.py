b80065118b01e0eaab54c08b7f3ec38ae74e5ed1
Degarashi <degarasu@gmail.com>
2025-05-04 18:29:36 +0900

    [add] edit_bmesh contextmanager

--- src/aux_other.py
+++ src/aux_other.py
@@ -1,3 +1,9 @@
+from contextlib import contextmanager
+
+import bmesh
+from bpy.types import Mesh
+
+
 class classproperty:
     def __init__(self, func):
         self.func = func
@@ -5,3 +11,18 @@ class classproperty:
     def __get__(self, instance, owner):
         return self.func(owner)
 
+
+# call in OBJECT-mode only
+@contextmanager
+def make_bmesh(mesh: Mesh, update_mesh: bool = True, recalc_normals: bool = True):
+    bm = bmesh.new()
+    bm.from_mesh(mesh)
+    try:
+        yield bm
+    finally:
+        if recalc_normals:
+            bm.normal_update()
+        if update_mesh:
+            bm.to_mesh(mesh)
+            mesh.update()
+        bm.free()
